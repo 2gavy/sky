818d784543a9cdaa56800c44e75130af451a5ed0
Zing Zai <ZLOO002@e.ntu.edu.sg>
2017-07-16 22:10:17 +0800

    Separated Panels for posts.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -29,23 +29,29 @@ class Home extends React.Component {
 
     return posts.map((post, index) => {
       return (
-        <TimelineView withHeader className={'border-hoverblue tl-' + post} key={index}>
-          <TimelineItem>
-            <TimelineHeader className={'bg-hover' + post}>
-              <TimelineIcon className={'bg-' + post + ' fg-white'} glyph='icon-fontello-chat-1' />
-              <TimelineTitle>
-                {now.getDate() + " " + month[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() + ":" + now.getMinutes()}
-              </TimelineTitle>
-            </TimelineHeader>
-            <TimelineBody>
-              <ul>
-                <li>
-                  <LoremIpsum query='2s' />
-                </li>
-              </ul>
-            </TimelineBody>
-          </TimelineItem>
-        </TimelineView>
+        <PanelContainer>
+          <Panel>
+            <PanelBody>
+              <TimelineView withHeader className={'border-hoverblue tl-' + post} key={index}>
+                <TimelineItem>
+                  <TimelineHeader className={'bg-hover' + post}>
+                    <TimelineIcon className={'bg-' + post + ' fg-white'} glyph='icon-fontello-chat-1' />
+                    <TimelineTitle>
+                      {now.getDate() + " " + month[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() + ":" + now.getMinutes()}
+                    </TimelineTitle>
+                  </TimelineHeader>
+                  <TimelineBody>
+                    <ul>
+                      <li>
+                        <LoremIpsum query='2s' />
+                      </li>
+                    </ul>
+                  </TimelineBody>
+                </TimelineItem>
+              </TimelineView>
+            </PanelBody>
+          </Panel>
+        </PanelContainer>
       );
     });
   }
@@ -64,13 +70,7 @@ class Home extends React.Component {
             </PanelContainer>
           </Col>
           <Col xs={4} sm={6} md={8}>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
-                  {this._getPosts()}
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
+            {this._getPosts()}
           </Col>
           <Col xs={4} sm={3} md={2}>
           </Col>
