c212ce538267d8077c8a7cccbb125347abd29178
alvin <alvin@local>
2017-10-19 17:10:28 +0800

    Fix USER_HOST_ADDR back to cloud

--- appConfig.js
+++ appConfig.js
@@ -1,5 +1,5 @@
-// export const USER_HOST_ADDR = 'http://35.198.222.39:3000/api/v1/';
-export const USER_HOST_ADDR = 'http://localhost:3000/api/v1/';
+export const USER_HOST_ADDR = 'http://35.198.222.39:3000/api/v1/';
+// export const USER_HOST_ADDR = 'http://localhost:3000/api/v1/';
 export const REPORT_BACKEND_HOST = 'http://35.187.236.166:8001/api';
 export const ELASTIC_SEARCH_HOST = 'http://35.198.247.221:9200/tap_overts';
 export const FRONTEND_ADDR = 'http://35.197.155.91:8080'
