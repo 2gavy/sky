a9f4e2c86d9dd035a54116d914d6d3a128e6150d
root <tap-modernwebapp@github.com>
2018-02-02 14:25:29 +0800

    Made changes to config file for ES to work locally

--- appConfig.js
+++ appConfig.js
@@ -7,8 +7,11 @@
 //IB
 // export const USER_HOST_ADDR = 'http://35.201.124.147/api/v1/';
 export const USER_HOST_ADDR = 'http://localhost:3000/api/v1/';
-export const REPORT_BACKEND_HOST = 'http://35.201.124.147/api';
-export const ELASTIC_SEARCH_HOST = 'http://35.198.247.221:9200/tap_overts';
-export const FRONTEND_ADDR = 'http://35.201.124.147/'
+//export const REPORT_BACKEND_HOST = 'http://35.201.124.147/api';
+export const REPORT_BACKEND_HOST = 'http://localhost:8001/api';
+//export const ELASTIC_SEARCH_HOST = 'http://35.198.247.221:9200/tap_overts';
+export const ELASTIC_SEARCH_HOST = 'http://192.168.2.251:9200/tap_overts';
+//export const FRONTEND_ADDR = 'http://35.201.124.147/'
+export const FRONTEND_ADDR = 'http://localhost:8080/'
 // export const COLLAB_BACKEND_HOST = 'http://localhost:8001/api/collab';
 export const COLLAB_BACKEND_HOST = 'http://192.168.2.247:5000';
